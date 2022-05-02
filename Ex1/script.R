af8c1f6e731efbffc79528d1d7dcdd5f33978d65
Judit <yuhalper@g.jct.ac.il>
2022-05-02 18:49:47 +0300

    data cube

--- Ex1/script.R
+++ Ex1/script.R
@@ -7,7 +7,10 @@ A$DateTime <- as.POSIXct( A$megawatthours, tz = "EST", "%H:%M EST %m/%d/%Y" )
 B <- A[ order(A$DateTime), ]
 
 # Choose Net.generation columns:
-C <- with(B, cbind(DateTime, Net.generation, Net.generation.1, Net.generation.2, Net.generation.3, Net.generation.4, Net.generation.5, Net.generation.6, Net.generation.7 , Net.generation.8 , Net.generation.9 , Net.generation.10))
+C <- with(B, cbind(Date = as.integer(format(as.Date(A[, 'DateTime']), "%Y%m%d")), BPAT = Net.generation, CISO	= Net.generation.1, CPLE = Net.generation.2, ERCO = Net.generation.3, FPL = Net.generation.4, ISNE = Net.generation.5, MISO = Net.generation.6,
+                         NYIS = Net.generation.7, PACW = Net.generation.8, PJM = Net.generation.9, US_Lower = Net.generation.10))
+
+#C <- with(B, cbind(DateTime, Net.generation, Net.generation.1, Net.generation.2, Net.generation.3, Net.generation.4, Net.generation.5, Net.generation.6, Net.generation.7 , Net.generation.8 , Net.generation.9 , Net.generation.10))
 
 # EDT -> PDT (-3 hrs):
 for (i in c(0, 1, 8)) {
