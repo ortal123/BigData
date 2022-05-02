397736a5a9080328770cfe68ff42ac2b9695fc93
Judit <yuhalper@g.jct.ac.il>
2022-05-02 17:03:19 +0300

    -1 hr

--- Ex1/script.R
+++ Ex1/script.R
@@ -6,7 +6,7 @@ A$DateTime <- as.POSIXct( A$megawatthours, tz = "EST", "%H:%M EST %m/%d/%Y" )
 # Order  by time:
 B <- A[ order(A$DateTime), ]
 
-# Choose Net.generation colomns:
+# Choose Net.generation columns:
 C <- with(B, cbind(DateTime, Net.generation, Net.generation.1, Net.generation.2, Net.generation.3, Net.generation.4, Net.generation.5, Net.generation.6, Net.generation.7 , Net.generation.8 , Net.generation.9 , Net.generation.10))
 
 # EDT -> PDT (-3 hrs):
@@ -19,19 +19,19 @@ for (i in c(0, 1, 8)) {
 # EDT -> CDT (-1 hrs): 
 for (i in c(3, 6)) {
   
-  C[, i + 2] <- c(C[(4 : length(C[,1])), i + 2], NA, NA, NA)
+  C[, i + 2] <- c(C[(2 : length(C[,1])), i + 2], NA)
   
 }
 
-# Slicing - 7-14 Feb:
-D <- C[which( C[, 'DateTime'] < as.POSIXct("2021-02-15 00:00:00 IST") & C[, 'DateTime'] >= as.POSIXct("2021-02-07 00:00:00 IST")), ]
+# Slicing - week of 7-14 Feb:
+D <- C[which( C[, 'DateTime'] < as.POSIXct("2021-02-15 00:00:00 EST") & C[, 'DateTime'] >= as.POSIXct("2021-02-07 00:00:00 EST")), ]
 
 # Roll-up - hours to days:
 
 
+strptime(D[1, "DateTime"], tz = "EST", format = "%d")
 
-
-
+as.POSIXct(strptime(D[2, "DateTime"],tz = "EST", format = "%d"))
 
 
 
